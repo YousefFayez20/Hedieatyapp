05e1c1790dcef09764648ae058091aab707ac1ff
YousefFayez20 <96171490+YousefFayez20@users.noreply.github.com>
2024-12-13 19:21:47 +0200

    Adding friends with profile picture with syncing from firebase

--- lib/pages/add_friend_dialog.dart
+++ lib/pages/add_friend_dialog.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import '../models/friend.dart';
-import '../utils/firestore_service.dart';  // Import FirestoreService
-import '../utils/database_helper.dart';   // Import DatabaseHelper
+import '../utils/firestore_service.dart';
+import '../utils/database_helper.dart';
+
 class AddFriendDialog extends StatefulWidget {
   final Function(Friend) onAdd;
-  final int userId; // userId is an integer passed to the dialog
+  final int userId;
 
-  const AddFriendDialog({Key? key, required this.onAdd, required this.userId}) : super(key: key);
+  const AddFriendDialog({Key? key, required this.onAdd, required this.userId})
+      : super(key: key);
 
   @override
   _AddFriendDialogState createState() => _AddFriendDialogState();
@@ -14,16 +16,14 @@ class AddFriendDialog extends StatefulWidget {
 
 class _AddFriendDialogState extends State<AddFriendDialog> {
   final _nameController = TextEditingController();
-  final _profileImageController = TextEditingController();
+  String? _selectedProfileImage;
   final _formKey = GlobalKey<FormState>();
   final FirestoreService _firestoreService = FirestoreService();
   final DatabaseHelper _dbHelper = DatabaseHelper();
 
-
   @override
   void dispose() {
     _nameController.dispose();
-    _profileImageController.dispose();
     super.dispose();
   }
 
@@ -32,84 +32,120 @@ class _AddFriendDialogState extends State<AddFriendDialog> {
       return;
     }
 
-    // Fetch the email using the userId (which is an integer)
-    final email = await DatabaseHelper().getEmailByUserId(widget.userId);
+    final email = await _dbHelper.getEmailByUserId(widget.userId);
 
     if (email == null) {
-      // If email is not found, show an error message and return
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('User not found.')));
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('User not found.')),
+      );
       return;
     }
 
     final newFriend = Friend(
       id: null,
       name: _nameController.text.trim(),
-      profileImage: _profileImageController.text.trim().isEmpty
-          ? 'assets/default_profile.png'
-          : _profileImageController.text.trim(),
+      profileImage: _selectedProfileImage ?? 'assets/default_profile.png',
       upcomingEvents: 0,
-      userId: widget.userId,  // Use the email retrieved from the database
+      userId: widget.userId,
     );
 
     try {
-
-      // Check if the friend already exists in the local database by firebase_id
-      if (newFriend.firebaseId != null) {
-        final existingFriend = await _dbHelper.fetchFriendByFirebaseId(newFriend.firebaseId!);
-        // Proceed with your logic
-      } else {
-        print("Error: Friend's firebaseId is null.");
-        // Handle the error (e.g., show a message, skip the operation)
-      }
-
-      //if (existingFriend != null) {
-        // If the friend already exists, show a message and don't insert again
-        //ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('This friend is already added.')));
-        //return;
-      //}
-
-      // Insert the new friend into the database
-      //final insertedId = await _dbHelper.insertFriend(newFriend);
       await _firestoreService.addFriendToFirestore(newFriend, email);
-
-      // After insertion, update the friend with the inserted ID
-      //final updatedFriend = newFriend.copyWith(id: insertedId);
-
-      // Call the callback to update the UI with the new friend
-      //widget.onAdd(updatedFriend);
-
-      // Close the dialog
+      widget.onAdd(newFriend);
       Navigator.of(context).pop();
     } catch (error) {
       print('Error adding friend: $error');
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Failed to add friend.')));
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Failed to add friend.')),
+      );
     }
+  }
+
+  Widget _buildImageSelector() {
+    final assetImages = [
+      'assets/man_2.png',
+      'assets/man_1.png',
+      'assets/girl_1.png',
+      'assets/girl_2.png'
+    ];
+
+    return GridView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        crossAxisSpacing: 8,
+        mainAxisSpacing: 8,
+      ),
+      itemCount: assetImages.length,
+      itemBuilder: (context, index) {
+        final imagePath = assetImages[index];
+        return GestureDetector(
+          onTap: () {
+            setState(() {
+              _selectedProfileImage = imagePath;
+            });
+          },
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Image.asset(
+                imagePath,
+                fit: BoxFit.cover,
+                width: 80,
+                height: 80,
+              ),
+              if (_selectedProfileImage == imagePath)
+                Container(
+                  width: 80,
+                  height: 80,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.green, width: 3),
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                  child: const Icon(Icons.check_circle, color: Colors.green),
+                ),
+            ],
+          ),
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+
     return AlertDialog(
       title: const Text('Add a Friend'),
-      content: Form(
-        key: _formKey,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            TextFormField(
-              controller: _nameController,
-              decoration: const InputDecoration(labelText: 'Friend Name'),
-              validator: (value) {
-                if (value == null || value.trim().isEmpty) {
-                  return 'Please enter a name.';
-                }
-                return null;
-              },
+      content: Container(
+        width: screenSize.width * 0.8, // Use 80% of the screen width
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextFormField(
+                  controller: _nameController,
+                  decoration: const InputDecoration(labelText: 'Friend Name'),
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Please enter a name.';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16),
+                const Text(
+                  'Select a Profile Picture:',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 8),
+                _buildImageSelector(),
+              ],
             ),
-            TextFormField(
-              controller: _profileImageController,
-              decoration: const InputDecoration(labelText: 'Profile Image URL (optional)'),
-            ),
-          ],
+          ),
         ),
       ),
       actions: [
@@ -124,4 +160,5 @@ class _AddFriendDialogState extends State<AddFriendDialog> {
       ],
     );
   }
+
 }
